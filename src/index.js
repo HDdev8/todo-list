d3dea68a786c2f2f0cd2057a52b77dba99c22832
HDdev8 <pantonakos@my.waketech.edu>
2023-02-19 01:33:00 -0500

    updating for gh-pages

--- src/index.js
+++ src/index.js
@@ -328,7 +328,7 @@ document.addEventListener("click", todoDeleteButtons);
 
 const removeStrikeThrough = (e) => {
   if (e.target.matches(`input[type="checkbox"]`) && !e.target.checked) {
-    e.target.checked;
+    e.target.checked === true;
     const sourceElement = e.target.parentElement.parentElement.parentElement;
     const dataId = sourceElement.dataset.id;
     const allChildren = document.querySelectorAll(
@@ -343,7 +343,7 @@ const removeStrikeThrough = (e) => {
 
 const addStrikeThrough = (e) => {
   if (e.target.matches(`input[type="checkbox"]`) && e.target.checked) {
-    !e.target.checked;
+    e.target.checked === false;
     const sourceElement = e.target.parentElement.parentElement.parentElement;
     const dataId = sourceElement.dataset.id;
     const allChildren = document.querySelectorAll(
